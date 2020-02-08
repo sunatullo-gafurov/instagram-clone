4c20851871fd9c970980c00829dd3f372553602e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-08 12:26:31 +0000

    Deploy sunatullo-gafurov/instagram-clone to github.com/sunatullo-gafurov/instagram-clone.git:gh-pages

---
+++ precache-manifest.5b1e72b8a01e2c22768cea870a651ca1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "174747f69cfee50bbadb4b1c352c7c37",
+    "url": "/instagram-clone/index.html"
+  },
+  {
+    "revision": "33414406f7171e3bb220",
+    "url": "/instagram-clone/static/css/main.d3f466e8.chunk.css"
+  },
+  {
+    "revision": "c635c10256ad3f385c0d",
+    "url": "/instagram-clone/static/js/2.f4e830fb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/instagram-clone/static/js/2.f4e830fb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "33414406f7171e3bb220",
+    "url": "/instagram-clone/static/js/main.12aa2ff5.chunk.js"
+  },
+  {
+    "revision": "0a6af5829d209cb1a842",
+    "url": "/instagram-clone/static/js/runtime-main.4a30d232.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9d221619ad09c1f04d9a5b5bddd66820.js"
+  "/instagram-clone/precache-manifest.5b1e72b8a01e2c22768cea870a651ca1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/instagram-clone/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
